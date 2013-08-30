ecaf0d73699254dbf37607e3b8fa20e5ae3e73f8
Ahmed El.Hussaini <aelhussaini@gmail.com>
2013-08-30 15:21:14 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', platforms: :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :test do
+  gem 'faker'
+  gem 'capybara'
+  gem 'fuubar'
+  # gem 'guard-rspec'
+  # gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OffersChallenge::Application.config.secret_token = '1a3d61c6a7d032b34a3283ae691df581e758df0591f9a8ba155b9a57c7f511ad5ff74ed30adeff286b6b0285302df7fdeb79c1dac4153892fcba9ab379ee2da5'
